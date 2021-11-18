c7e504b0f102ce9f89d3f260cde33faa75ca4ef5
Ondřej Herian <ondra.heri@gmail.com>
2021-11-18 16:31:38 +0100

    Section 1,2 Update

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const btnSrollTo = document.querySelector('.btn--scroll-to');
+const section01 = document.querySelector('#section--1');
 
 const openModal = function (e) {
   e.preventDefault(); // if this would not be there, then href='#' on btn--show-modal would move whole page up to top.
@@ -29,3 +31,44 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+btnSrollTo.addEventListener('click', e => {
+  section01.scrollIntoView({ behavior: 'smooth' });
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    e.target.classList.contains('nav__link') &&
+    !e.target.classList.contains('btn--show-modal')
+  ) {
+    const id = e.target.getAttribute('href');
+    document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+  }
+});
+// console.log(document.querySelector('.nav__link').getAttribute('href')); ---- relative path
+// console.log(document.querySelector('.nav__link').href); -------------------- absolute path
+
+document.querySelector('.header__img').style.transform = 'scale(1.2)';
+
+// Functionality for Tabbed component
+const tabsButtons = document.querySelectorAll('.operations__tab');
+const tabsContent = document.querySelectorAll('.operations__content');
+document
+  .querySelector('.operations__tab-container')
+  .addEventListener('click', function (e) {
+    const el = e.target.closest('.btn');
+    if (!el) {
+      return;
+    }
+    // Remove all active classes
+    tabsButtons.forEach(c => c.classList.remove('operations__tab--active'));
+    tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+    // Add active classes to selected Element
+    el.classList.add('operations__tab--active');
+    document
+      .querySelector(`.operations__content--${el.getAttribute('data-tab')}`)
+      .classList.add('operations__content--active');
+  });
